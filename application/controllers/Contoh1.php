174f1f4a09a76313420efdb6fc4d3bc34b8a38c5
teguhramdhani <teguhramdhani062@gmail.com>
2022-03-22 14:50:05 +0700

    menambahkan file Contoh1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,10 +1,10 @@
 <?php
 class Contoh1 extends CI_Controller
 {
- public function index()
- {
- echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Teguh Ramdhani<br>
- Saya tinggal di daerah Teluknaga, Tangerang. <br> Saya seorang wibu & woota.";
- }
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Teguh Ramdhani<br>
+ Saya tinggal di daerah Teluknaga, Tangerang. <br> Saya seorang Mahasiswa.";
+    }
 }
